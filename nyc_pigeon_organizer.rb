d1d4e9ee4b97d553f1d7ff9663ca8abcde96cbc7
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-19 20:33:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,14 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   result = { }
-  data.each do |attribute, value| #runs through outer hash through all keys and values. values are hashes..
+  data.each do |attribute, value| 
     value.each do |key, names|
       names.each do |names|
         if !result[names]
           result[names] = {}
         end
 
-        if result[names]
+        if !result[names][attribute]
           result[names][attribute] = []
         end
 
