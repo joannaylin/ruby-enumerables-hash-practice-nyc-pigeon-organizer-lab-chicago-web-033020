1911258ea6300113536dc4b325270a5e627e3b8b
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-19 18:45:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  result = { }
+  i = 0
+  while i < data.length
+    characteristic = data[i]
+    characteristic.map { |key, value|
+      [k, v.to_s].to_h
+    }
+  end
 end
